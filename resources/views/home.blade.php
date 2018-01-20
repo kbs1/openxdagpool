ec5f65e178e1bf146e4ba2124fc13bed7d18f082
Viliam Kubis <viliam.kubis@gmail.com>
2018-01-20 21:27:29 +0100

    added balance checker UI tooltip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -154,7 +154,7 @@
 										<input class="input is-fullwidth" type="text" name="address" placeholder="Wallet address" maxlength="32" required>
 									</div>
 									<div class="control">
-										<button class="button" type="submit">
+										<button class="button tooltip" data-tooltip="Balance updates every 4 hours." type="submit">
 											Show
 										</button>
 									</div>
