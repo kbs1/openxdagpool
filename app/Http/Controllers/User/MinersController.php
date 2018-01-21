7772a90932b862b980438693475f1e9ed47d4743
Ing. Viliam Kubis <viliam.kubis@gmail.com>
2018-01-21 06:11:36 +0100

    user miners control panel - insert real miner status when inserting miner to prevent "back online" notification when adding the miner

--- app/Http/Controllers/User/MinersController.php
+++ app/Http/Controllers/User/MinersController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateMiner;
 
+use App\Pool\DataReader;
+use App\Pool\Miners\Parser as MinersParser;
+
 use App\Miners\Miner;
 use Auth;
 
@@ -31,6 +34,7 @@ class MinersController extends Controller
 			'address' => $request->input('address'),
 			'note' => $request->input('note'),
 			'email_alerts' => false,
+			'seen_online' => false,
 		]);
 
 		$miner->user_id = $user->id;
@@ -53,9 +57,10 @@ class MinersController extends Controller
 		return redirect()->back()->with('success', 'Miner successfully deleted.');
 	}
 
-	public function alerts(Request $request)
+	public function alerts(Request $request, DataReader $reader)
 	{
 		$user = Auth::user();
+		$miners_parser = null;
 
 		foreach ((array) $request->input('alerts') as $uuid => $alert) {
 			$miner = Miner::where('uuid', $uuid)->where('user_id', $user->id)->first();
@@ -63,6 +68,12 @@ class MinersController extends Controller
 			if (!$miner) continue;
 
 			$miner->email_alerts = $alert ? true : false;
+
+			if ($alert) {
+				$miners_parser = $miners_parser ?? new MinersParser($reader->getMiners());
+				$miner->seen_online = $miners_parser->getMiner($miner->address) ? true : false;
+			}
+
 			$miner->save();
 		}
 
