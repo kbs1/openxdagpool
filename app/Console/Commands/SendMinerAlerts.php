1c612bf67b958170f7c0bc21f20fb0b32493027a
Viliam Kubis <viliam.kubis@gmail.com>
2018-01-24 18:15:40 +0100

    don't send miner down alerts if pool software crashes

--- app/Console/Commands/SendMinerAlerts.php
+++ app/Console/Commands/SendMinerAlerts.php
@@ -7,6 +7,9 @@ use Illuminate\Console\Command;
 use App\Miners\Miner;
 use App\Mail\{MinerWentOffline, MinerBackOnline};
 
+use App\Pool\DataReader;
+use App\Pool\Statistics\Parser as StatisticsParser;
+
 use Illuminate\Support\Facades\Mail;
 
 class SendMinerAlerts extends Command
@@ -14,8 +17,25 @@ class SendMinerAlerts extends Command
 	protected $signature = 'alerts:miners';
 	protected $description = 'Check each registered miner and send went-offline or back-online notification whenever applicable';
 
+	protected $reader;
+
+	public function __construct(DataReader $reader)
+	{
+		$this->reader = $reader;
+		parent::__construct();
+	}
+
 	public function handle()
 	{
+		$stats_parser = new StatisticsParser($this->reader->getStatistics());
+		$pool_hashrate = (float) $stats_parser->getPoolHashrate();
+
+		if ($pool_hashrate == 0) {
+			$this->line('Zero pool hashrate, not sending alerts.');
+			$this->info('SendMinerAlerts completed successfully.');
+			return;
+		}
+
 		foreach (Miner::where('email_alerts', true)->get() as $miner) {
 			if ($miner->status === 'offline' && $miner->seen_online) {
 				$this->line("Sending 'went offline' notification for miner '{$miner->address}' to '{$miner->user->email}'...");
