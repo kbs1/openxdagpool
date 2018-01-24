2835afe23b41f5656d5f9e14c1e295ff87d5207c
Ing. Viliam Kubis <viliam.kubis@gmail.com>
2018-01-24 07:14:16 +0100

    further optimised payments importer

--- app/Console/Commands/ImportPayouts.php
+++ app/Console/Commands/ImportPayouts.php
@@ -32,28 +32,21 @@ class ImportPayouts extends Command
 		$last_made_at = null;
 		$insert = [];
 
-		$payouts_parser->forEachPayoutLine(function(PoolPayout $pool_payout) use ($latest, $latest_fully_imported_at, &$last_made_at, &$insert) {
+		$payouts_parser->forEachPayoutLine(function(PoolPayout $pool_payout) use ($latest_fully_imported_at, &$last_made_at, &$insert) {
 			$made_at = $pool_payout->getMadeAt();
 
 			if ($latest_fully_imported_at && $made_at <= $latest_fully_imported_at)
 				return;
 
 			if ($latest_fully_imported_at && !$last_made_at) {
-				Payout::where('made_at', '<', $made_at)->orWhere(function($query) use ($made_at) {
-					$query->where('made_at', '=', $made_at)->where('made_at_milliseconds', '<', floor($made_at->micro / 1000));
-				})->update([
-					'date_fully_imported' => true,
-				]);
-
 				Payout::where('made_at', '=', $made_at)->where('made_at_milliseconds', '=', floor($made_at->micro / 1000))->delete();
 			}
 
 			$last_made_at = $last_made_at ?? $made_at;
 
 			if ($last_made_at < $made_at) {
-				\DB::table('payouts')->update([
-					'date_fully_imported' => true,
-				]);
+				if ($latest_fully_imported_at)
+					\DB::table('payouts')->where('date_fully_imported', false)->update(['date_fully_imported' => true]);
 
 				Payout::insert($insert);
 				$insert = [];
@@ -67,16 +60,22 @@ class ImportPayouts extends Command
 				'sender' => $pool_payout->getSender(),
 				'recipient' => $pool_payout->getRecipient(),
 				'amount' => $pool_payout->getAmount(),
-				'date_fully_imported' => false,
+				'date_fully_imported' => $latest_fully_imported_at ? false : true,
 				'created_at' => $now = Carbon::now()->format('Y-m-d H:i:s'),
 				'updated_at' => $now,
 			];
 		});
 
 		if ($insert) {
-			\DB::table('payouts')->update([
-				'date_fully_imported' => true,
-			]);
+			if ($latest_fully_imported_at) {
+				\DB::table('payouts')->where('date_fully_imported', false)->update(['date_fully_imported' => true]);
+			} else {
+				foreach ($insert as &$ins)
+					$ins['date_fully_imported'] = false;
+
+				unset($ins);
+			}
+
 			Payout::insert($insert);
 		}
 
