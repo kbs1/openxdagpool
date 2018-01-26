0fcaa856747ea2b0e6843efc6a214704ecdf623d
Viliam Kubis <viliam.kubis@gmail.com>
2018-01-26 22:42:03 +0100

    leaderboard fix

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -56,7 +56,7 @@
 									@php ($myself_rank = $index + 1)
 									@php ($myself_hashrate = $item['hashrate'])
 								@endif
-								@if (!$shown_full && !$shown_myself && isset($authUser))
+								@if (!$shown_full && !$shown_myself && isset($authUser) && !$authUser->exclude_from_leaderboard)
 									@php ($shown_full = true)
 									<tr>
 										<td>...</td>
