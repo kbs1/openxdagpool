da268a7b90186e0f44eb62e45da274bd73f7dd9f
Ing. Viliam Kubis <viliam.kubis@gmail.com>
2018-02-01 13:24:35 +0100

    fees wording change

--- app/Pool/Config.php
+++ app/Pool/Config.php
@@ -11,6 +11,6 @@ class Config
 
 	public function getConfig()
 	{
-		return env('FEES_PERCENT', 0.5) . '% pool fee, ' . env('REWARD_PERCENT', 12) . '% reward for connected miners, ' . env('DIRECT_PERCENT', 7) . '% reward for found block, ' . env('FUND_PERCENT', 0.5) . '% donation to community fund';
+		return env('FEES_PERCENT', 0.5) . '% pool fee, ' . env('REWARD_PERCENT', 12) . '% reward for block contribution, ' . env('DIRECT_PERCENT', 7) . '% reward for found block, ' . env('FUND_PERCENT', 0.5) . '% donation to community fund';
 	}
 }
