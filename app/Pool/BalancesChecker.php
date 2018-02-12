4a074bfc8ae1e3225df6ac192b59b771c4f6bbba
Ing. Viliam Kubis <viliam.kubis@gmail.com>
2018-02-12 19:17:39 +0100

    added the option to disable balance checking

--- app/Pool/BalancesChecker.php
+++ app/Pool/BalancesChecker.php
@@ -11,7 +11,10 @@ class BalancesChecker
 		if (isset($this->balances[$address]))
 			return $this->balances[$address];
 
-		$result = @file_get_contents(sprintf(env('BALANCE_CHECKER_URL'), urlencode($address)));
+		if (trim(env('BALANCE_CHECKER_URL')) === '')
+			return null;
+
+		$result = @file_get_contents(sprintf(trim(env('BALANCE_CHECKER_URL')), urlencode($address)));
 
 		if ($result && preg_match('/\d+\.\d{9}/siu', $result, $match))
 			return $this->balances[$address] = $match[0];
