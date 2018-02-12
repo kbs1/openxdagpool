03baeb9028a0f3189de3ab5dc579eafb87d70772
Ing. Viliam Kubis <viliam.kubis@gmail.com>
2018-02-12 05:40:40 +0100

    admin layout update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
-@section('flashMessagesWidth', '10')
+@section('flashMessagesWidth', '8')
 
 @section('content')
 	<div class="admin-view">
 		<div class="columns is-marginless is-centered">
-			<div class="column is-10">
+			<div class="column is-8">
 				<div class="tabs">
 					<ul>
 						<li{!! $section == 'users' ? ' class="is-active"' : '' !!}><a href="{{ route('user.admin.users') }}">Users</a></li>
