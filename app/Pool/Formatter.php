b5591c32544a42265c71e7f8f448ad07da0d2d98
Viliam Kubis <viliam.kubis@gmail.com>
2018-03-02 18:43:36 +0100

    code format update

--- app/Pool/Formatter.php
+++ app/Pool/Formatter.php
@@ -24,8 +24,8 @@ class Formatter
 		$units = ['h/s', 'Kh/s', 'Mh/s', 'Gh/s', 'Th/s', 'Ph/s', 'Eh/s', 'Zh/s', 'Yh/s'];
 		$unit = intval(log(abs(intval($rate)), 1024));
 
-        if (array_key_exists($unit, $units))
-            return sprintf('%.2f %s', $rate / pow(1024, $unit), $units[$unit]);
+		if (array_key_exists($unit, $units))
+			return sprintf('%.2f %s', $rate / pow(1024, $unit), $units[$unit]);
 
 		return $rate;
 	}
