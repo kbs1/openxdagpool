b6fbb0da7c3158c3e9affa44c3062768af3c110b
Viliam Kubis <viliam.kubis@gmail.com>
2018-04-20 18:27:59 +0200

    SaveMinerStats: don't update address balance to zero if we are currently unable to obtain the value

--- app/Console/Commands/SaveMinerStats.php
+++ app/Console/Commands/SaveMinerStats.php
@@ -41,7 +41,13 @@ class SaveMinerStats extends Command
 			$balance = $miner->balance;
 
 			if (!$miner->balance_updated_at || $miner->balance_updated_at <= Carbon::now()->subMinutes(30)) {
-				$balance = (float) $this->balances->getBalance($miner->address);
+				$balance = $this->balances->getBalance($miner->address);
+
+				if ($balance === null)
+					$balance = $miner->balance; // don't update balance to zero if we are currently unable to obtain it
+				else
+					$balance = (float) $balance;
+
 				$miner->balance_updated_at = Carbon::now();
 			}
 
